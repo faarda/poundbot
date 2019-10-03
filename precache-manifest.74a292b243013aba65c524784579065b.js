8589ee982f9a16aeb32a194cdc2da74a65a40b5c
Silas Adedoyin <silasfaarda@gmail.com>
2019-10-03 04:24:46 +0100

    Updates

---
+++ precache-manifest.74a292b243013aba65c524784579065b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "da815226df527a62abec7e1fa561821f",
+    "url": "/style.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c60f84a461ee5f0b3cc7",
+    "url": "/js/chunk-vendors.c080d274.js"
+  },
+  {
+    "revision": "9f2f526dd9eebfe8b313",
+    "url": "/js/app.77338b2b.js"
+  },
+  {
+    "revision": "2ca7f725457a55b7a372",
+    "url": "/js/about.5aea4956.js"
+  },
+  {
+    "revision": "c3c23b92e593c5616890864cc0d706de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca535981a4dc6951172e8132e8aebf9d",
+    "url": "/img/pound-logo.ca535981.png"
+  },
+  {
+    "revision": "fb749a2f2751fb60487a6fc8481cb1ab",
+    "url": "/img/loading.fb749a2f.gif"
+  },
+  {
+    "revision": "333a002ca05dc14ecb62a2255f3ff995",
+    "url": "/favicon.png"
+  }
+];
